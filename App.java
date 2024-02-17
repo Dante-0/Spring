c680d300bd90761e4b397a10f146a20227cbc718
Ayush Singh <160009745+Dante-0@users.noreply.github.com>
2024-02-17 22:22:33 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,21 @@
+package com.springproject.collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App {
+			
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springproject/collection/config.xml");
+		
+		Student st = context.getBean("st1", Student.class);
+		
+		System.out.println(st);
+		
+	}
+	
+	
+}
